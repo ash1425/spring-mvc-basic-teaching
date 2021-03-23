5f766285d619ac77c8f26e2abe99c5f21467208d
Ashay Thorat <ashayt@thoughtworks.com>
2021-03-23 23:53:40 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WelcomeController extends AbstractController {
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView modelAndView = new ModelAndView("Hello");
+        modelAndView.addObject("msg", "hello world");
+        return modelAndView;
+    }
+}
